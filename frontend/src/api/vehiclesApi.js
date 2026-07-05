9cf3827f57a3de2ca72bf12230d87583e22d9d75
Taimur Hasan <thayvaro79@gmail.com>
2026-07-05 07:17:33 -0700

    Point frontend to Azure API

--- frontend/src/api/vehiclesApi.js
+++ frontend/src/api/vehiclesApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5146/api";
+const API_BASE_URL = "https://revart-api-79.azurewebsites.net/api";
 
 export async function getVehicles() {
   const response = await axios.get(`${API_BASE_URL}/Vehicles`);
